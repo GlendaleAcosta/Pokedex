bf75974234a008e141c571c5c17c6e7a986ff7c4
Glendeezy <gglendale17@gmail.com>
2016-09-28 20:26:48 -0700

    Added stackview layout to Pokemon Bio UI and back functionality

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,44 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Glendale Acosta on 9/28/16.
+//  Copyright © 2016 Glendale Acosta. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
+    
+    var pokemon: Pokemon!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       nameLbl.text = pokemon.name.capitalized
+    }
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
