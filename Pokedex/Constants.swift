77d5fb9cea6b4f902b0ea312c3f22f400cd52233
Glendeezy <gglendale17@gmail.com>
2016-09-30 20:33:38 -0700

    added AlamoFire and API calls to bio page

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Glendale Acosta on 9/30/16.
+//  Copyright © 2016 Glendale Acosta. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
